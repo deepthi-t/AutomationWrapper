5fcd4915e6317479e0ccdb41f690bf52ab9d3d58
Nikhil Das <nikhil.dastp@gmail.com>
2017-05-30 16:58:53 +0530

    WIP ManageSwitchRequest class

--- LiberateAutomation/src/com/liberate/automation/pom/ManageSwitchRequest.java
+++ LiberateAutomation/src/com/liberate/automation/pom/ManageSwitchRequest.java
@@ -6,106 +6,126 @@ import com.liberate.automation.common.LeftLink;
 import com.liberate.automation.common.LiberateCommon;
 import com.liberate.automation.core.TestActions;
 
-public class ManageSwitchRequest
-{
+public class ManageSwitchRequest {
 	TestActions action = null;
-	
-	//Page Elements : Column Headers
-	By ManageSwitchRequestColumnHeader		= By.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Query Switch Request']");
-	By MonitorNewSwitchRequestsColumnHeader = By.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Monitor New Switch Requests']");
-	
-	//Page Elements : Input Fields
-	By QuerySwitchRequestDropdown	= By.xpath("//*[text()='Query Switch Request:']/following::select");
-	By EmployeeNameIDDropDown		= By.xpath("//*[text()='Employee Name / ID:']/following::select[1]");
-	By EmployeeNameIDSearchBox		= By.xpath("//*[text()='Employee Name / ID:']/following::input[1]");
-	By UpdatedWithinLast			= By.xpath("//*[text()='Updated within last:']/following::input[1]");
-	
-	//Page Elements : Command Buttons
-	By SearchButton	= By.xpath("//input[@value='Search']");
-	By ResetButon	= By.xpath("//input[@value='Reset']");
-	
-	public ManageSwitchRequest(TestActions action)
-	{
+
+	// Data
+	public String monitorSwitchReq_Status = "";
+
+	// Page Elements : Column Headers
+	By ManageSwitchRequest_ColumnHeader = By
+			.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Query Switch Request']");
+	By MonitorNewSwitchRequests_ColumnHeader = By
+			.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Monitor New Switch Requests']");
+	By FailureQueries_ColumnHeader = By
+			.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Failure Queries']");
+	By SwitchRequestSummary_ColumnHeader = By
+			.xpath("//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Switch Request Summary']");
+	By SwitchRequestSummaryDetails_ColumnHeader = By.xpath(
+			"//div[@class='icePnlClpsbl icePnlClpsblDM']/descendant::*[text()='Switch Request Summary Details']");
+
+	// Page Elements : Input Fields
+	By QuerySwitchRequestDropdown = By.xpath("//*[text()='Query Switch Request:']/following::select");
+
+	// Page Elements : Monitor New Switch Request
+	By EmployeeNameID_Input = By.xpath("//*[text()='Employee Name / ID:']/following::input[1]");
+	By EmployeeNameID_SearchButton = By.xpath("//*[text()='Employee Name / ID:']/following::input[@type='image']");
+	By EmployeeNameID_DropDown = By.xpath("//*[text()='Employee Name / ID:']/following::select[1]");
+	By UpdatedWithinLast_Input = By.xpath("//*[text()='Updated within last:']/following::input[1]");
+	By Status_DropDown = By.xpath("//*[text()='Status:']/following::select[1]");
+
+	// Page Elements : Failure Queries
+	By SwitchID_DropDown = By.xpath("//*[text()='Switch ID:']/following::select[1]");
+	By Region_DropDown = By.xpath("//*[text()='Region:']/following::select[1]");
+	By DepartmentCode_DropDown = By.xpath("//*[text()='Department Code:']/following::select[1]");
+	By TargetExchangeID_DropDown = By.xpath("//*[text()='Target Exch ID :']/following::select[1]");
+	By StartDate_Input = By.xpath("//*[text()='Start Date:']/following::input[1]");
+	By EndDate_Input = By.xpath("//*[text()='End Date:']/following::input[1]");
+
+	// Page Elements : Command Buttons
+	By SearchButton = By.xpath("//input[@value='Search']");
+	By ResetButon = By.xpath("//input[@value='Reset']");
+
+	public ManageSwitchRequest(TestActions action) {
 		this.action = action;
 	}
-	
-	public boolean navigate()
-	{
+
+	public boolean navigate() {
 		boolean passed = false;
-		
+
 		passed = action.clickOn(LiberateCommon.LevelOne.Orders);
 		passed = action.waitFor(LiberateCommon.Orders.SwitchRequests, 4, true);
 		passed = action.clickOn(LiberateCommon.Orders.SwitchRequests);
-		passed = action.waitFor(LeftLink.SwitchRequests.ManageSwitchRequest,4,true);
+		passed = action.waitFor(LeftLink.SwitchRequests.ManageSwitchRequest, 4, true);
 		passed = action.clickOn(LeftLink.SwitchRequests.ManageSwitchRequest);
-		
-		return passed;
-	}
-	
-	public boolean select_MonitorNewSwitchRequests()
-	{
-		boolean passed = false;
-		
-		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
-		passed = action.SelectBy(QuerySwitchRequestDropdown, "Monitor New Switch Requests");
-		
-		return passed;
-	}
-	
-	public boolean select_FailureQueries()
-	{
-		boolean passed = false;
-		
-		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
-		passed = action.SelectBy(QuerySwitchRequestDropdown, "Failure Queries");
-		
-		return passed;
-	}
-	
-	public boolean select_QuerybyAcctServiceOrderServiceCommand()
-	{
-		boolean passed = false;
-		
-		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
-		passed = action.SelectBy(QuerySwitchRequestDropdown, "Query by Acct, Service Order, Service or Command");
-		
-		return passed;
-	}
-	
-	public boolean select_GeneralQueryOnNewRequests()
-	{
-		boolean passed = false;
-		
-		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
-		passed = action.SelectBy(QuerySwitchRequestDropdown, "General Query on New Requests");
-		
-		return passed;
-	}
-	
-	public boolean select_GenerateSwitchRequest()
-	{
-		boolean passed = false;
-		
-		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
-		passed = action.SelectBy(QuerySwitchRequestDropdown, "Generate Switch Request");
-		
+
 		return passed;
 	}
 
-	public boolean MonitorNewSwitchRequests_Search(String employeeID, String updatedWithinLast)
-	{
+	public boolean select_MonitorNewSwitchRequests() {
 		boolean passed = false;
-		
-		passed = action.waitFor(MonitorNewSwitchRequestsColumnHeader, 4, true);
-		if(action.getSelectedOption(EmployeeNameIDDropDown).contains("Select"))
-		{
-			passed = action.sendDataTo(EmployeeNameIDSearchBox, employeeID);
+
+		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
+		passed = action.SelectBy(QuerySwitchRequestDropdown, "Monitor New Switch Requests");
+
+		return passed;
+	}
+
+	public boolean select_FailureQueries() {
+		boolean passed = false;
+
+		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
+		passed = action.SelectBy(QuerySwitchRequestDropdown, "Failure Queries");
+
+		return passed;
+	}
+
+	public boolean select_QuerybyAcctServiceOrderServiceCommand() {
+		boolean passed = false;
+
+		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
+		passed = action.SelectBy(QuerySwitchRequestDropdown, "Query by Acct, Service Order, Service or Command");
+
+		return passed;
+	}
+
+	public boolean select_GeneralQueryOnNewRequests() {
+		boolean passed = false;
+
+		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
+		passed = action.SelectBy(QuerySwitchRequestDropdown, "General Query on New Requests");
+
+		return passed;
+	}
+
+	public boolean select_GenerateSwitchRequest() {
+		boolean passed = false;
+
+		passed = action.waitFor(QuerySwitchRequestDropdown, 4, true);
+		passed = action.SelectBy(QuerySwitchRequestDropdown, "Generate Switch Request");
+
+		return passed;
+	}
+
+	public boolean MonitorNewSwitchRequests_Search(String employeeID, String updatedWithinLast) {
+		boolean passed = false;
+
+		passed = action.waitFor(MonitorNewSwitchRequests_ColumnHeader, 4, true);
+		if (action.getSelectedOption(EmployeeNameID_DropDown).contains("Select")) {
+			passed = action.sendDataTo(EmployeeNameID_Input, employeeID);
+			passed = action.clickOn(EmployeeNameID_SearchButton);
 			passed = action.waitFor(2);
-			passed = action.SelectBy(EmployeeNameIDDropDown, 1);
+			passed = action.SelectBy(EmployeeNameID_DropDown, 1);
 		}
-		
-		passed = action.sendDataTo(UpdatedWithinLast, updatedWithinLast);
-		
+
+		if (!monitorSwitchReq_Status.equals(""))
+			passed = action.SelectBy(Status_DropDown, monitorSwitchReq_Status);
+
+		passed = action.sendDataTo(UpdatedWithinLast_Input, updatedWithinLast);
+		passed = action.clickOn(SearchButton);
+
+		// TODO verification of result
+
 		return passed;
 	}
 }
