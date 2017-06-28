3d589e5803df27466d2715a2414c91cc6bf409e8
Nikhil Das <nikhil.dastp@gmail.com>
2017-06-28 13:44:42 +0530

    Converted Switch Request test cases to TestNG classes

--- LiberateAutomation/src/com/liberate/automation/main/Main.java
+++ LiberateAutomation/src/com/liberate/automation/main/Main.java
@@ -1,7 +1,6 @@
 package com.liberate.automation.main;
 
 import com.liberate.automation.core.TestActions;
-import com.liberate.automation.testcases.SwitchRequest;
 
 public class Main {
 
@@ -9,10 +8,6 @@ public class Main {
 		// TODO Auto-generated method stub
 
 		TestActions action = new TestActions();
-		action.gotoURL("http://172.21.73.80:8081/liberate-LONI01-S10/");
-		SwitchRequest.FailureQueries(action);
-		SwitchRequest.generatQueryOnSwitchRequest(action);
-		SwitchRequest.queryByCustomerDetails_AccountNumber(action);
 		
 		action.closeTab();
 		action.quit();
--- LiberateAutomation/src/com/liberate/automation/testcases/SwitchRequest.java
+++ LiberateAutomation/src/com/liberate/automation/testcases/SwitchRequest.java
@@ -1,84 +1,103 @@
 package com.liberate.automation.testcases;
 
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 import com.liberate.automation.core.TestActions;
 import com.liberate.automation.pom.Login;
 import com.liberate.automation.pom.ManageSwitchRequest;
 
 public class SwitchRequest {
-
-	public static boolean MonitorNewSwitchRequest(TestActions action) {
-
-		ManageSwitchRequest msr = new ManageSwitchRequest(action);
-		Login login = new Login(action);
-
-		login.login("libadmin", "Ic3cr34m!");
-		msr.navigate();
-		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.select_MonitorNewSwitchRequests();
-		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.MonitorNewSwitchRequests_Search("99999", "23:30");
-		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.verifyResults();
-		action.getScreenShot("MonitorNewSwitchRequest");
-
-		return true;
-	}
-
-	public static boolean FailureQueries(TestActions action) {
-		ManageSwitchRequest msr = new ManageSwitchRequest(action);
-		Login login = new Login(action);
-
-		login.login("libadmin", "Ic3cr34m!");
-		action.getScreenShot("FailureQueries");
-		msr.navigate();
-		action.getScreenShot("FailureQueries");
-		msr.select_FailureQueries();
-		action.getScreenShot("FailureQueries");
-		msr.FailureQueries("FLOW-FLOW TV Gateway Interface", "CAY-Cayman Islands Region", "FLOW-FLOW", "",
-				"31/05/2015 03:15", "31/05/2017 03:15");
-		action.getScreenShot("FailureQueries");
-		msr.verifyResults();
-		action.getScreenShot("MonitorNewSwitchRequest");
-
-		return true;
+	
+	static TestActions action = new TestActions();
+	
+	@BeforeSuite
+	public static void navigateToURL()
+	{
+		action.gotoURL("http://172.21.73.80:8081/liberate-LONI01-S10/");
 	}
 	
-	public static boolean queryByCustomerDetails_AccountNumber(TestActions action)
+	@AfterSuite
+	public static void endTesting()
+	{
+		action.closeTab();
+		action.quit();
+		action = null;
+	}
+	
+	@Test(enabled = false)
+	public static void MonitorNewSwitchRequest() {
+
+		ManageSwitchRequest msr = new ManageSwitchRequest(action);
+		Login login = new Login(action);
+		
+		assertEquals(login.login("libadmin", "Ic3cr34m!"),true);
+		assertEquals(msr.navigate(),true);
+		action.getScreenShot("MonitorNewSwitchRequest");
+		assertEquals(msr.select_MonitorNewSwitchRequests(),true);
+		action.getScreenShot("MonitorNewSwitchRequest");
+		assertEquals(msr.MonitorNewSwitchRequests_Search("99999", "23:30"),true);
+		action.getScreenShot("MonitorNewSwitchRequest");
+		assertEquals(msr.verifyResults(),true);
+		action.getScreenShot("MonitorNewSwitchRequest");
+	}
+	
+	@Test
+	public static void FailureQueries() {
+		ManageSwitchRequest msr = new ManageSwitchRequest(action);
+		Login login = new Login(action);
+
+		assertEquals(login.login("libadmin", "Ic3cr34m!"),true);
+		action.getScreenShot("FailureQueries");
+		assertEquals(msr.navigate(),true);
+		action.getScreenShot("FailureQueries");
+		assertEquals(msr.select_FailureQueries(),true);
+		action.getScreenShot("FailureQueries");
+		assertEquals(msr.FailureQueries("FLOW-FLOW TV Gateway Interface", "CAY-Cayman Islands Region", "FLOW-FLOW", "",
+				"31/05/2015 03:15", "31/05/2017 03:15"),true);
+		action.getScreenShot("FailureQueries");
+		assertEquals(msr.verifyResults(),true);
+		action.getScreenShot("MonitorNewSwitchRequest");
+
+	}
+	
+	@Test
+	public static void queryByCustomerDetails_AccountNumber()
 	{
 		ManageSwitchRequest msr = new ManageSwitchRequest(action);
 		Login login = new Login(action);
 		
-		login.login("libadmin", "Ic3cr34m!");
+		assertEquals(login.login("libadmin", "Ic3cr34m!"),true);
 		action.getScreenShot("queryByCustomerDetails_AccountNumber");
-		msr.navigate();
+		assertEquals(msr.navigate(),true);
 		action.getScreenShot("queryByCustomerDetails_AccountNumber");
-		msr.select_QuerybyAcctServiceOrderServiceCommand();
+		assertEquals(msr.select_QuerybyAcctServiceOrderServiceCommand(),true);
 		action.getScreenShot("queryByCustomerDetails_AccountNumber");
-		msr.QueryByCustomerDetails("260002430000", "", "", "");
+		assertEquals(msr.QueryByCustomerDetails("260002430000", "", "", ""),true);
 		action.getScreenShot("queryByCustomerDetails_AccountNumber");
-		msr.verifyResults();
+		assertEquals(msr.verifyResults(),true);
 		action.getScreenShot("queryByCustomerDetails_AccountNumber");
-
-		return true;
 	}
 	
-	public static boolean generatQueryOnSwitchRequest(TestActions action)
+	@Test
+	public static void generatQueryOnSwitchRequest()
 	{
 		ManageSwitchRequest msr = new ManageSwitchRequest(action);
 		Login login = new Login(action);
 		
-		login.login("libadmin", "Ic3cr34m!");
+		assertEquals(login.login("libadmin", "Ic3cr34m!"),true);
 		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.navigate();
+		assertEquals(msr.navigate(),true);
 		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.select_GeneralQueryOnNewRequests();
+		assertEquals(msr.select_GeneralQueryOnNewRequests(),true);
 		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.GeneralQUery("C-Complete", "FLOW-FLOW TV Gateway Interface", "CAY-Cayman Islands Region", "FLOW-FLOW", "",
-				"31/05/2015 03:15", "31/05/2017 03:15");
+		assertEquals(msr.GeneralQUery("C-Complete", "FLOW-FLOW TV Gateway Interface", "CAY-Cayman Islands Region", "FLOW-FLOW", "",
+				"31/05/2015 03:15", "31/05/2017 03:15"),true);
 		action.getScreenShot("MonitorNewSwitchRequest");
-		msr.verifyResults();
+		assertEquals(msr.verifyResults(),true);
 		action.getScreenShot("MonitorNewSwitchRequest");
-
-		return true;
 	}
 }
