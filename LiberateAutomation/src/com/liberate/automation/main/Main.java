a52fd8d1c0f7b323b3d3f272995a8cfc57ff67d1
Nikhil Das <nikhil.dastp@gmail.com>
2017-07-14 12:28:17 +0530

    Added Testng Configuraiton

--- LiberateAutomation/src/com/liberate/automation/main/Main.java
+++ LiberateAutomation/src/com/liberate/automation/main/Main.java
@@ -1,11 +1,8 @@
 package com.liberate.automation.main;
 
-import test.goIbibo;
-
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		goIbibo.login();
-	}
+}
 }
