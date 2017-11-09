17e64c3832e9ea74fc59ccb3916c9a1db4916cd6
Nikhil Das <nikhil.dastp@gmail.com>
2017-11-09 14:58:45 +0530

    Fixed coding standard issues

--- LiberateAutomation/src/com/liberate/automation/core/RandomData.java
+++ LiberateAutomation/src/com/liberate/automation/core/RandomData.java
@@ -6,7 +6,14 @@ import java.util.Objects;
 import java.util.Random;
 
 public class RandomData {
-
+    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String digits = "0123456789";
+    public static final String lower = upper.toLowerCase(Locale.ROOT);
+    public static final String alphanum = upper + lower + digits;
+    private final Random random;
+    private final char[] symbols;
+    private final char[] buf;
+    
     /**
      * Generate a random string.
      */
@@ -15,20 +22,6 @@ public class RandomData {
             buf[idx] = symbols[random.nextInt(symbols.length)];
         return new String(buf);
     }
-
-    public static final String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-    public static final String lower = upper.toLowerCase(Locale.ROOT);
-
-    public static final String digits = "0123456789";
-
-    public static final String alphanum = upper + lower + digits;
-
-    private final Random random;
-
-    private final char[] symbols;
-
-    private final char[] buf;
 
     public RandomData(int length, Random random, String symbols) {
         if (length < 1) throw new IllegalArgumentException();
