25a46f612621f5aa76b4da6dc26d6ee72e0b598e
Nikhil Das <nikhil.dastp@gmail.com>
2017-11-10 12:10:17 +0530

    Resolving Conflicts

--- LiberateAutomation/src/com/liberate/automation/main/Main.java
+++ LiberateAutomation/src/com/liberate/automation/main/Main.java
@@ -1,22 +1,13 @@
 package com.liberate.automation.main;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.openqa.selenium.By;
-
-import com.liberate.automation.core.TestActions;
-import com.liberate.automation.pom.Login;
 import com.liberate.automation.testcases.RaiseFaultTestCases;
 
 public class Main {
 
 	public static void main(String[] args) {
-	
-		RaiseFaultTestCases rftc=new RaiseFaultTestCases();
+
+		RaiseFaultTestCases rftc = new RaiseFaultTestCases();
 		rftc.navigateToURL();
 		rftc.raiseNewFault();
-}
+	}
 }
