f702482e629b4bf4b224a3423fd80027e35b3f35
Nikhil Das <nikhil.dastp@gmail.com>
2018-01-15 18:49:20 +0530

    Credit Control Service Status Completed

--- LiberateAutomation/src/com/liberate/automation/common/CommonData.java
+++ LiberateAutomation/src/com/liberate/automation/common/CommonData.java
@@ -1,9 +1,8 @@
 package com.liberate.automation.common;
 
 public class CommonData {
-	//Data Classes
-	public static class ApplicationForm
-	{
+	// Data Classes
+	public static class ApplicationForm {
 		public final static String MobileprepaidResidential = "Mobile prepaid(Residential)";
 		public final static String MobileprepaidBusiness = "Mobile prepaid(Business)";
 		public final static String MobilepostpaidResidential = "Mobile postpaid(Residential)";
@@ -12,14 +11,12 @@ public class CommonData {
 		public final static String FixedBusiness = "Fixed(Business)";
 	}
 
-	public static class CustomerType
-	{
+	public static class CustomerType {
 		public final static String Business = "B - Business";
-		public final static String Residential = "R - Residential";	
+		public final static String Residential = "R - Residential";
 	}
-	
-	public static class AddressType
-	{
+
+	public static class AddressType {
 		public final static String PostCode = "Post Code";
 		public final static String StandardAddress = "A-Standard Address";
 		public final static String ForeignAddress = "F-Foreign Address";
@@ -27,10 +24,15 @@ public class CommonData {
 		public final static String PanamaReverseDropdown = "Q-Panama Reverse Dropdown";
 		public final static String StreetAddress = "S-Street Address";
 	}
-	
-	public static class PlantItemType
-	{
+
+	public static class PlantItemType {
 		public final static String CopperDP = "DP-DISTRIBUTION";
 		public final static String FiberDP = "FDP";
+	}
+
+	public static class CreditControlServiceStatus {
+		public final static String ROS = "ROS";
+		public final static String BAR = "BAR";
+		public final static String TOS = "TOS";
 	}
 }
