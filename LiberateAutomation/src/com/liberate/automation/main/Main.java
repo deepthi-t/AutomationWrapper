c64c816d712a6a13f983ca9fb67b090278ea8ab1
Nikhil Das <nikhil.dastp@gmail.com>
2018-02-06 14:47:39 +0530

    Bad cheque proccessing #30 Fixed

--- LiberateAutomation/src/com/liberate/automation/main/Main.java
+++ LiberateAutomation/src/com/liberate/automation/main/Main.java
@@ -1,14 +1,14 @@
 package com.liberate.automation.main;
 
+import com.liberate.automation.testcases.BadChequeProcessing;
 import com.liberate.automation.testcases.CommonLogin;
-import com.liberate.automation.testcases.CustomerCareTC;
 
 public class Main {
 
 	public static void main(String[] args) {
 //		while (true) {
 			CommonLogin.navigateToURL();
-			CustomerCareTC.suspendService();
+			BadChequeProcessing.badCheckProcessing();
 //			CommonLogin.endTesting();
 //		}
 	}
