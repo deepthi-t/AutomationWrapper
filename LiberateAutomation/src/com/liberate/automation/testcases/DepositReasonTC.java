a13b8d9ccd5da5575a1a4829151ba6649097b85f
Nikhil Das <nikhil.dastp@gmail.com>
2018-02-12 14:20:32 +0530

    Renamed Class and removed unused methods

---
+++ LiberateAutomation/src/com/liberate/automation/testcases/DepositReasonTC.java
@@ -0,0 +1,38 @@
+package com.liberate.automation.testcases;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import com.liberate.automation.core.RandomData;
+import com.liberate.automation.core.TestActions;
+import com.liberate.automation.pom.MaintainDepositReason;
+
+public class DepositReasonTC {
+
+	static TestActions action = CommonLogin.action;
+
+	@Test
+	public static void createNewDepositReason() {
+		String TestCase = "DepositReason_createNewDepositReason";
+
+		RandomData random = new RandomData();
+		MaintainDepositReason mdr = new MaintainDepositReason(action);
+
+		action.getScreenShot(TestCase);
+		assertEquals(mdr.navigate(), true);
+		action.getScreenShot(TestCase);
+		assertEquals(mdr.clickNewButton(), true);
+		action.getScreenShot(TestCase);
+		assertEquals(mdr.provideDepositReasonDetailsNewCreation(random.nextString().substring(4), "TestingAutomation",
+				"Y", "4"), true);
+		action.getScreenShot(TestCase);
+		assertEquals(mdr.clickAcceptButton(), true);
+		action.getScreenShot(TestCase);
+		action.waitFor(By.xpath("//span[@class='iceMsgInfo']"), 2, true);
+		String x = action.getTextFromPage(By.xpath("//span[@class='iceMsgInfo']"));
+		action.getScreenShot(TestCase);
+		System.out.println(x);
+	}
+}
